7389d8dade5c62b7fbd717aef3c9bf2daae9da60
Jose Marcos <jkmarcosb@gmail.com>
2022-05-11 19:07:22 +0000

    Get Data from POST Requests

--- myApp.js
+++ myApp.js
@@ -1,13 +1,17 @@
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser');
+
 console.log("Hello World");
-console.log(process.env.MESSAGE_STYLE);
 
 app.use('/public', express.static(__dirname + '/public'));
+
 app.use(function(req, res, next) {
 	console.log(`${req.method} ${req.path} - ${req.ip}`);
 	next();
 });
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/views/index.html');
@@ -17,6 +21,28 @@ app.get('/json', function(req, res) {
 	res.json({"message": 'uppercase' == process.env.MESSAGE_STYLE 
 						? "Hello json".toUpperCase()
 						: "Hello json"});
+});
+
+app.get('/now', function(req, res, next) {
+	req.time = new Date().toString();
+	next();
+}, function(req, res) {
+	res.json({time: req.time});
+});
+
+app.get('/:word/echo', function(req, res) {
+	console.log(req.params.word);
+	res.json({echo: req.params.word});
+});
+
+app.route('/name').get(function(req, res) {
+	console.log(req.query);
+	const { first, last } = req.query;
+	res.json({ name: `${first} ${last}`});
+}).post(function(req, res){
+	console.info(req.body);
+	const { first, last } = req.body;
+	res.json({ name: `${first} ${last}`});
 });
 
 
