9e819213011a42ed7911eaabf730352e966e045e
Jose Marcos <jkmarcosb@gmail.com>
2022-05-11 18:22:03 +0000

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,39 +1,25 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+console.log(process.env.MESSAGE_STYLE);
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', function(req, res) {
+	res.json({"message": 'uppercase' == process.env.MESSAGE_STYLE 
+						? "Hello json".toUpperCase()
+						: "Hello json"});
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
